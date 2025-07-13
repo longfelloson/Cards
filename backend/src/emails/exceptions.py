4489bcd8d1942746a461907ec9d02c9178630da9
Ivan Kashirin <kashirinivan@icloud.com>
2025-07-13 16:18:19 +0200

    feat(emails): add logging for send_email errors

--- backend/src/emails/exceptions.py
+++ backend/src/emails/exceptions.py
@@ -1,11 +1,3 @@
-from fastapi import HTTPException, status
-
-
-class EmailSendingFailedException(HTTPException):
-    def __init__(
-        self,
-        status_code: int = status.HTTP_500_INTERNAL_SERVER_ERROR,
-        detail: str = "Failed to send verification email",
-        headers=None,
-    ):
-        super().__init__(status_code, detail, headers)
+class EmailSendException(Exception):
+    def __init__(self, detail: str = "Email send failed"):
+        super().__init__(detail)
--- backend/src/emails/utils.py
+++ backend/src/emails/utils.py
@@ -1,15 +1,23 @@
 from email.message import EmailMessage
 
 import aiosmtplib
+
+from emails.exceptions import EmailSendException
 from config import settings
+from logger import logger
 
 
 async def send_email(message: EmailMessage) -> None:
-    await aiosmtplib.send(
-        message,
-        hostname=settings.smtp.SMTP_HOST,
-        port=settings.smtp.SMTP_PORT,
-        start_tls=True,
-        username=settings.smtp.SMTP_EMAIL,
-        password=settings.smtp.SMTP_PASSWORD.get_secret_value(),
-    )
+    try:
+        await aiosmtplib.send(
+            message,
+            hostname=settings.smtp.SMTP_HOST,
+            port=settings.smtp.SMTP_PORT,
+            start_tls=True,
+            username=settings.smtp.SMTP_EMAIL,
+            password=settings.smtp.SMTP_PASSWORD.get_secret_value(),
+        )
+    except aiosmtplib.SMTPException as e:
+        logger.error(msg="Error while sending a message via email", exc_info=True)
+        raise EmailSendException() from e
+    
