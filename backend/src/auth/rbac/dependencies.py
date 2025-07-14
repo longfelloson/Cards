d61efabe2326e36771aed3719d0340ca69097d90
Ivan Kashirin <kashirinivan@icloud.com>
2025-07-14 11:41:21 +0200

    fix(rbac): fix user id resource check

--- backend/src/auth/rbac/dependencies.py
+++ backend/src/auth/rbac/dependencies.py
@@ -1,8 +1,8 @@
+from fastapi import Request, Depends
 from auth.dependencies import get_current_user
+from users.models import User, Role
 from auth.rbac.exceptions import AccessDeniedException
-from auth.rbac.utils import get_request_resource, has_access_to_resource
-from fastapi import Depends, Request
-from users.models import User
+from auth.rbac.utils import has_access_to_resource, get_request_resource
 
 USER_ID_KEY = "user_id"
 
@@ -11,13 +11,14 @@ async def check_permissions(
     request: Request,
     current_user: User = Depends(get_current_user),
 ):
+    if current_user.role == Role.ADMIN:
+        return
+
+    current_user_id = str(current_user.id)
     path_user_id = request.path_params.get(USER_ID_KEY)
     query_user_id = request.query_params.get(USER_ID_KEY)
 
-    if path_user_id and path_user_id != str(current_user.id):
-        raise AccessDeniedException()
-
-    if query_user_id and query_user_id != str(current_user.id):
+    if not (path_user_id == current_user_id or query_user_id == current_user_id):
         raise AccessDeniedException()
 
     resource = get_request_resource(request.url.path)
