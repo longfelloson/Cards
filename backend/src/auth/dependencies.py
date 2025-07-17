0431c5079ac93e19a38d36c0ca69f38f2d9b02fb
Ivan Kashirin <kashirinivan@icloud.com>
2025-07-17 14:57:38 +0200

    fix: imports

--- backend/src/auth/dependencies.py
+++ backend/src/auth/dependencies.py
@@ -1,12 +1,10 @@
-from typing import Annotated
+from typing import Annotated, Sequence
 
 from auth.exceptions import InvalidTokenError
 from auth.utils import get_user_from_token
 from auth.permissions import BasePermission
 from fastapi import Depends, Request
 from users.models import User
-from users.schemas import UserFilter
-from users.service import users_service
 
 
 async def get_current_user(
