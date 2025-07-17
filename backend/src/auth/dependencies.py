1892f72bb869658bc6cc6d58fa3ac6161d5167ac
Ivan Kashirin <kashirinivan@icloud.com>
2025-07-17 14:47:50 +0200

    feat: add getting the token from cookies

--- backend/src/auth/dependencies.py
+++ backend/src/auth/dependencies.py
@@ -1,24 +1,22 @@
 from typing import Annotated
 
-from auth.exceptions import InvalidTokenException
-from auth.token import decode_token, oauth2_scheme
-from dependencies import UOWDependency
-from fastapi import Depends
+from auth.exceptions import InvalidTokenError
+from auth.utils import get_user_from_token
+from auth.permissions import BasePermission
+from fastapi import Depends, Request
 from users.models import User
 from users.schemas import UserFilter
 from users.service import users_service
 
 
 async def get_current_user(
-    uow: UOWDependency,
-    token: str = Depends(oauth2_scheme),
+    request: Request,
 ) -> User:
     """Get a current user from JWT by a request"""
-    token_payload = decode_token(token)
-    user_filter = UserFilter(email=token_payload["sub"])
-    user = await users_service.get_by(filter=user_filter, uow=uow)
+    token = request.cookies.get("token")
+    user = await get_user_from_token(token, request.state.uow)
     if not user:
-        raise InvalidTokenException()
+        raise InvalidTokenError()
     return user
 
 
