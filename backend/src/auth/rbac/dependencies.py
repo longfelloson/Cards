d8a7e70d684d86a9f3273cd4dc345722b3db6590
Ivan Kashirin <kashirinivan@icloud.com>
2025-07-17 14:43:48 +0200

    feat: change RBAC dependency

--- backend/src/auth/rbac/dependencies.py
+++ backend/src/auth/rbac/dependencies.py
@@ -1,29 +1,14 @@
-from fastapi import Request, Depends
-from auth.dependencies import get_current_user
-from users.models import User, Role
-from auth.rbac.exceptions import AccessDeniedException
-from auth.rbac.utils import has_access_to_resource, get_request_resource
+from fastapi import Request
 
-USER_ID_KEY = "user_id"
+from auth.rbac.enums import Role
+from auth.rbac.exceptions import ResourceNotFound
+from auth.rbac.utils import has_access_to_resource
 
 
-async def check_permissions(
-    request: Request,
-    current_user: User = Depends(get_current_user),
-):
-    if current_user.role == Role.ADMIN:
-        return
+class RoleAccessDependency:
+    async def __call__(self, request: Request):
+        if request.user.role == Role.ADMIN:
+            return
 
-    current_user_id = str(current_user.id)
-    path_user_id = request.path_params.get(USER_ID_KEY)
-    query_user_id = request.query_params.get(USER_ID_KEY)
-
-    if path_user_id or current_user_id:
-        if not (path_user_id == current_user_id or query_user_id == current_user_id):
-            raise AccessDeniedException()
-
-    resource = get_request_resource(request.url.path)
-    action = request.method.casefold()
-
-    if not has_access_to_resource(current_user, resource, action):
-        raise AccessDeniedException()
+        if not has_access_to_resource(request, request.user):
+            raise ResourceNotFound()
