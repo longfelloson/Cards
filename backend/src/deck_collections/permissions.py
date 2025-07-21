6be18c4ca4d2bf119fb39b3a95271b5d5239b723
Ivan Kashirin <kashirinivan@icloud.com>
2025-07-21 16:29:17 +0200

    feat: add CollectionViewPermission

--- backend/src/deck_collections/permissions.py
+++ backend/src/deck_collections/permissions.py
@@ -1,6 +1,11 @@
 from fastapi import Request
 
-from auth.permissions import BasePermission
+from auth.permissions import (
+    BasePermission,
+    OwnerPermission,
+    VisibilityPermission,
+    any_permission,
+)
 from deck_collections.service import collections_service
 
 
@@ -12,3 +17,23 @@ class CollectionOwnerPermission(BasePermission):
         )
 
         return collection.user_id == request.user.id
+
+
+class CollectionViewPermission(BasePermission):
+    async def has_required_permissions(self, request: Request) -> bool:
+        """
+        Check if user owns a collection or requires visible one
+        """
+        collection_id = request.path_params["collection_id"]
+        collection = await collections_service.get(
+            collection_id=collection_id, uow=request.state.uow
+        )
+
+        has_permission = await any_permission(
+            permissions=[
+                OwnerPermission(instance=collection),
+                VisibilityPermission(instance=collection),
+            ],
+            request=request,
+        )
+        return has_permission
