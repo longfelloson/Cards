895b57ce3d90319939294be3c4a95feb2b8b830e
Ivan Kashirin <kashirinivan@icloud.com>
2025-07-22 14:07:15 +0200

    feat: add UserOwnerPermission

--- backend/src/users/permissions.py
+++ backend/src/users/permissions.py
@@ -1,16 +1,12 @@
 from fastapi import Request
 
 from auth.permissions import BasePermission
-from auth.rbac.enums import Role
-from users.service import users_service
 
 
-class UserViewPermission(BasePermission):
+class UserOwnerPermission(BasePermission):
     async def has_required_permissions(self, request: Request) -> bool:
-        if request.user.role == Role.ADMIN:
-            return True
-
-        user_id = request.path_params["user_id"]
-        user = await users_service.get(user_id=user_id, uow=request.state.uow)
-
-        return user.id == request.user.id
+        """
+        Check if user requests his own resource
+        """
+        user_id = request.path_params['user_id']
+        return str(request.user.id) == user_id
