94e37e985e38194779d12b7223faf76320b26257
Ivan Kashirin <kashirinivan@icloud.com>
2025-07-24 15:50:50 +0200

    feat: change keys to static values

--- backend/src/cache/keys.py
+++ backend/src/cache/keys.py
@@ -1,40 +1,18 @@
 from enum import StrEnum
 
-from pydantic import UUID4
-
 from cache.namespaces import Namespace
-from config import settings
 
 
 class Key(StrEnum):
-    @classmethod
-    def deck(cls, deck_id: UUID4) -> str:
-        return f"{settings.redis.DB_PREFIX}:{Namespace.DECK}:{deck_id}"
-
-    @classmethod
-    def decks(cls) -> str:
-        return f"{settings.redis.DB_PREFIX}:{Namespace.DECKS}"
-
-    @classmethod
-    def user(cls, user_id: UUID4) -> str:
-        return f"{settings.redis.DB_PREFIX}:{Namespace.USER}:{user_id}"
-
-    @classmethod
-    def users(cls) -> str:
-        return f"{settings.redis.DB_PREFIX}:{Namespace.USERS}"
-
-    @classmethod
-    def collection(cls, collection_id: UUID4) -> str:
-        return f"{settings.redis.DB_PREFIX}:{Namespace.COLLECTION}:{collection_id}"
-
-    @classmethod
-    def collections(cls) -> str:
-        return f"{settings.redis.DB_PREFIX}:{Namespace.COLLECTIONS}"
-
-    @classmethod
-    def card(cls, card_id: UUID4) -> str:
-        return f"{settings.redis.DB_PREFIX}:{Namespace.CARD}:{card_id}"
-
-    @classmethod
-    def cards(cls) -> str:
-        return f"{settings.redis.DB_PREFIX}:{Namespace.CARDS}"
+    DECK = Namespace.DECK + ":{deck_id}"
+    DECKS = Namespace.DECKS + ":{filter}"
+    
+    CARD = Namespace.CARD + ":{card_id}"
+    CARDS = Namespace.CARDS + ":{filter}"
+    
+    USER = Namespace.USER + ":{user_id}"
+    USERS = Namespace.USERS + ":{filter}"
+    
+    COLLECTION = Namespace.COLLECTION + ":{collection_id}"
+    COLLECTIONS = Namespace.COLLECTIONS + ":{filter}"
+    
