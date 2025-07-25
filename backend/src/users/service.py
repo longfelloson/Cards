58ad7ad1c3cc0e99b4f46e6a1d67c93fd2a15467
Ivan Kashirin <kashirinivan@icloud.com>
2025-07-25 13:51:22 +0200

    feat: add logging to users service

--- backend/src/users/service.py
+++ backend/src/users/service.py
@@ -1,13 +1,14 @@
 from typing import List, Optional
 
 from pydantic import UUID4
+from sqlalchemy.exc import SQLAlchemyError
 
 from auth.password import get_hashed_password
 from service import AbstractService
-from unit_of_work import UnitOfWork
 from users.exceptions import UserAlreadyExistsException, UserNotFoundException
 from users.models import User
 from users.schemas import UserCreate, UserFilter, UsersFilter, UserUpdate
+from logger import logger
 
 
 class UsersService(AbstractService):
@@ -19,38 +20,56 @@ class UsersService(AbstractService):
     async def create(self, *, data: UserCreate) -> User:
         """Creates a user with provided data"""
         data.password = get_hashed_password(data.password)
-        async with self.uow:
-            user_filter = UserFilter(email=data.email)
-            user = await self.get_by(filter=user_filter)
-            if user:
-                raise UserAlreadyExistsException()
-            user = await self.uow.users.create(data=data)
+        try:
+            async with self.uow:
+                user_filter = UserFilter(email=data.email)
+                user = await self.get_by(filter=user_filter)
+                if user:
+                    raise UserAlreadyExistsException()
+                user = await self.uow.users.create(data=data)
 
-            await self.uow.commit()
-            await self.clear_user_related_cache(user.id)
-
+                await self.uow.commit()
+        except SQLAlchemyError as exc:
+            logger.error(f"Failed to create a user with data = {data}:", exc_info=True)
+            raise exc
+        else:
             return user
 
     async def get(self, *, user_id: UUID4) -> User:
         """Get a user by its ID"""
-        async with self.uow:
-            user = await self.uow.users.get(id=user_id)
-            if not user:
-                raise UserNotFoundException()
+        try:
+            async with self.uow:
+                user = await self.uow.users.get(id=user_id)
+                if not user:
+                    raise UserNotFoundException()
+        except SQLAlchemyError as exc:
+            logger.error(
+                f"Failed to get a user with user_id = {user_id}:", exc_info=True
+            )
+            raise exc
+        else:
             return user
 
     async def get_by(self, *, filter: UserFilter) -> Optional[User]:
         """Get a user by provided filter"""
-        async with self.uow:
-            user = await self.uow.users.get_by(filter=filter)
+        try:
+            async with self.uow:
+                user = await self.uow.users.get_by(filter=filter)
+        except SQLAlchemyError as exc:
+            logger.error(f"Failed to get a user with filter = {filter}:", exc_info=True)
+            raise exc
+        else:
             return user
 
-    async def get_all(
-        self, *, filter: UsersFilter, uow: UnitOfWork
-    ) -> List[Optional[User]]:
+    async def get_all(self, *, filter: UsersFilter) -> List[Optional[User]]:
         """Get users by provided conditions"""
-        async with uow:
-            users = await uow.users.get_all(filter=filter)
+        try:
+            async with self.uow:
+                users = await self.uow.users.get_all(filter=filter)
+        except SQLAlchemyError as exc:
+            logger.error(f"Failed to get users with filter = {filter}:", exc_info=True)
+            raise exc
+        else:
             return users
 
     async def update(self, *, user_id: UUID4, data: UserUpdate) -> Optional[User]:
@@ -62,40 +81,48 @@ class UsersService(AbstractService):
             User: updated user if fields to update were provided otherwise the
             same user
         """
-        async with self.uow:
-            user = await self.get(user_id=user_id)
-            if data.verify:
-                await self.verification_service.verify(
-                    user=user,
-                    new_email=data.email,
-                    new_password=data.password,
-                )
-                return user
-            elif data.verification_token:
-                await self.verification_service.confirm(token=data.verification_token)
+        try:
+            async with self.uow:
+                user = await self.get(user_id=user_id)
+                if data.verify:
+                    await self.verification_service.verify(
+                        user=user,
+                        new_email=data.email,
+                        new_password=data.password,
+                    )
+                    return user
+                elif data.verification_token:
+                    await self.verification_service.confirm(
+                        token=data.verification_token
+                    )
 
-            if data.password:
-                data.password = get_hashed_password(data.password)
+                if data.password:
+                    data.password = get_hashed_password(data.password)
 
-            if not data.are_new_column_values_provided(user):
-                return user
+                if not data.are_new_column_values_provided(user):
+                    return user
 
-            update_data = data.model_dump(exclude_none=True)
-            updated_user = await self.uow.users.update(obj=user, data=update_data)
+                update_data = data.model_dump(exclude_none=True)
+                updated_user = await self.uow.users.update(obj=user, data=update_data)
 
-            await self.commit()
-            await self.clear_user_related_cache(user_id)
-
+                await self.commit()
+        except SQLAlchemyError as exc:
+            logger.error(
+                f"Failed to update a user with data = {data} and user_id = {user_id}:",
+                exc_info=True,
+            )
+            raise exc
+        else:
             return updated_user
 
     async def delete(self, *, user_id: UUID4) -> None:
         """Delete a user by its id"""
-        async with self.uow:
-            await self.uow.users.delete(obj_id=user_id)
-            await self.uow.commit()
-            await self.clear_user_related_cache(user_id)
-
-    async def clear_user_related_cache(self, user_id: UUID4) -> None:
-        await self.storage.clear_cache_by_keys(
-            self.storage.keys.user(user_id), self.storage.keys.users()
-        )
+        try:
+            async with self.uow:
+                await self.uow.users.delete(obj_id=user_id)
+                await self.uow.commit()
+        except SQLAlchemyError as exc:
+            logger.error(
+                f"Failed to delete a user with user_id = {user_id}:", exc_info=True
+            )
+            raise exc
